dc232c586ae60dcd4e3255d75e1e33cb376e5947
vrjalsko <diorvelonjara@gmail.com>
2024-01-18 13:19:35 +0300

    update 13:19

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Home = () => {
+  return (
+
+    <div className="bg-green-500 text-white p-4">
+
+
+        <button className='p-4 text-white'>Itineraire</button>
+      <h2 className="text-2xl font-bold">Wild Life Exploration in Andasibe -Maruxi</h2>
+      <p className="mt-2">This is a basic example component.</p>
+    </div>
+  )
+}
+
+export default Home
